f360e57d39b4f8b7dff74b8c87e168343d03fafc
MominovDEV <mardonmominov6@gmail.com>
2023-07-14 16:32:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("hello typescrpt");
+console.log("salom dunyo");
+throw new Error("some errror");
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,6 @@
+console.log("hello typescrpt");
+
+console.log("salom dunyo");
+
+
+throw new Error("some errror");
